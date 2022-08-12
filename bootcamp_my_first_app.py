3a213267732adcca1036a990581fb95423993502
pablonoya <pablonoya18@live.com>
2022-08-11 21:26:05 -0400

    change tabs to expanders

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,24 +2,11 @@ import streamlit as st
 
 st.header('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.caption('Simple but effective tips for every python lovers')
-st.text("""The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.
+st.write("""The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.
 
 In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.""")
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10 = st.tabs([
-    "1. Walrus operator",
-    "2. Splitting a string",
-    "3. Reversing a string",
-    "4. Merging two dictionaries",
-    "5. The zip() function",
-    "6. Assigning multiple list values to a variable",
-    "7. Remove duplicate list items",
-    "8. Lambda function",
-    "9. Swapping variable value",
-    "10. Use a password in your code",
-    ])
-
-with tab1:
+with st.expander("1. Walrus operator"):
     st.markdown("""The `Walrus` or `:=` operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.""")
     st.markdown("""**Example**
     If we want to check and print the length of a list:
@@ -31,7 +18,7 @@ print(l)""")
     ```3```
     """)
 
-with tab2:
+with st.expander("2. Splitting a string"):
     st.markdown("""If you want to split the components of a string into a list you can do that easily using the split() function in python. This will make the string operations a lot easier!""")
     st.markdown("""**Example**""")
     st.code("""string = “hello world”
@@ -41,7 +28,7 @@ string.split()
     ```['hello', 'world']```
     """)
 
-with tab3:
+with st.expander("3. Reversing a string"):
     st.markdown("""If you want to reverse a given string, you can do that with only one line of code using the negative indexing of the string.""")
     st.markdown("""**Example**""")
     st.code("""str="hello world!"
@@ -52,7 +39,7 @@ print(a)
     ```!dlrow olleh```
     """)
 
-with tab4:
+with st.expander("4. Merging two dictionaries"):
     st.markdown("""This amazing trick will help you merge two dictionaries with just 1 line of code. We just need to use `**` in front of the name of the two dictionaries like below two merge them into a single dictionary:""")
     st.markdown("""**Example**""")
     st.code("""d1 = {“a”: 10, “b”:20}
@@ -64,7 +51,7 @@ print(d3)
     ```{'a': 10, 'b': 20, 'c': 30, 'd': 40}```
     """)
 
-with tab5:
+with st.expander("5. The zip() function"):
     st.markdown("""The `zip()` function in python can make your life a lot easier when working with lists and dictionaries. It is used to combine several lists of the same length.""")
     st.markdown("""**Example**""")
     st.code("""
@@ -90,7 +77,7 @@ print(dictionary)
     ```{'Rajesh': 87, 'kumar': 90, 'Kriti': 88}```
     """)
 
-with tab6:
+with st.expander("6. Assigning multiple list values to a variable"):
     st.markdown("""If you want to assign some specific values of a list to a variable and all the remaining values to another variable in a list format, you can use the following technique:""")
     st.markdown("""**Example**""")
     st.code("""mylist = [1,2,3,4,5]
@@ -102,7 +89,8 @@ print(”b =", b)
     ```a = 1
 b = [2, 3, 4, 5]```
     """)
-with tab7:
+
+with st.expander("7. Remove duplicate list items",):
     st.markdown("""Do you have duplicate items in your list which you want to remove? You can do that with only one line of code using the set() function.""")
     st.markdown("""**Example**""")
     st.code("""mylist = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 7, 8, 9]
@@ -112,7 +100,8 @@ print(newlist)
     st.markdown("""**Output**  
     ```{1, 2, 3, 4, 5, 6, 7, 8, 9}```
     """)
-with tab8:
+
+with st.expander("8. Lambda function"):
     st.markdown("""If you need a function that is not very complicated, it can be done easily in one line using **lambda**. They are also called anonymous functions and are used heavily in data science and web development.""")
     st.markdown("""**Example**  
 Let’s say you want to write a function to multiply two numbers. Instead of writing a conventional function, you can do that in one line using:
@@ -124,7 +113,7 @@ mul(5, 6)
     ```30```
     """)
 
-with tab9:
+with st.expander("9. Swapping variable value"):
     st.markdown("""Do you have duplicate items in your list which you want to remove? You can do that with only one line of code using the set() function.""")
     st.markdown("""**Example**""")
     st.code("""a = 100
@@ -138,7 +127,7 @@ print('b = ', b)
 b = 100```
     """)
 
-with tab10:
+with st.expander("10. Use a password in your code"):
     st.markdown("""This python trick is amazing for securing your code with a password. We will use the `getpass()` function from the library `getpass` which encodes your input. This will prevent anyone from running the code without a password. Isn’t that cool!""")
     st.markdown("""**Example**""")
     st.code("""from getpass import getpass
@@ -154,5 +143,3 @@ Welcome stranger!
 Password: **** [zxcv]
 Wrong password```
     """)
-
-
